78f25450cf43a02a4bc7a7c1d853e9c730e02a64
valeriasolorza <v.solorza.343@gmail.comcd>
2024-04-15 19:33:10 -0700

    basics

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-console.log(canvas)
+var c = canvas.getContext('2d');
+
+const colors = ['red', 'blue', 'yellow', 'green']
+let sequence = []
+let userInput = []
+let score = 0
+
+function addToSequence(){
+    let newColor = colors[Math.floor(Math.random() * colors.length)];
+    sequence.push(newColor)
+    console.log(sequence)
+}
+
+addToSequence();
+
+addToSequence();
+addToSequence();
